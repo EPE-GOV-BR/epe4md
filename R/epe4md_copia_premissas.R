1353f08f5ad5593386f78b95f851397e81fdcc01
Gabriel Konzen <gabrielkonzen@hotmail.com>
2023-01-24 15:49:29 -0300

    função copia premissas com parametrização do destino

--- R/epe4md_copia_premissas.R
+++ R/epe4md_copia_premissas.R
@@ -1,9 +1,11 @@
-#' Copia as planilhas de premissas originais do modelo para um novo diretório
-#' chamado dados_premissas, na raiz do projeto atual.
+#' Copia as planilhas de premissas originais do modelo para um novo diretório.
 #'
 #' @param ano_base numeric. Ano base das premissas que serão copiadas.
+#' @param destino string. Diretório de destino das planilhas. Se não for
+#' preenchido, a função cria uma pasta chamada dados_premissas na raiz do
+#' projeto atual do RStudio.
 #'
-#' @return data.frame. Base tratada e resumida.
+#' @return Cópia das planilhas de premissas.
 #' @export
 #'
 #'@encoding UTF-8
@@ -12,21 +14,22 @@
 #'
 
 epe4md_copia_premissas <- function(
-    ano_base
+    ano_base,
+    destino = "dados_premissas"
 ) {
   
-  if (file.exists(paste0("dados_premissas/", ano_base))) {
+  if (file.exists(paste0(destino, "/", ano_base))) {
     
     } else {
     
-  dir.create(paste0("dados_premissas/", ano_base), recursive = TRUE)
+      dir.create(paste0(destino, "/", ano_base), recursive = TRUE)
       
     }
   
   arquivos <- list.files(paste0(system.file(package = "epe4md"), "/dados_premissas/", ano_base)) 
   
   file.copy(from = paste0(system.file(package = "epe4md"), "/dados_premissas/", ano_base, "/", arquivos),   
-            to = paste0("dados_premissas/", ano_base, "/", arquivos), overwrite = TRUE)
+            to = paste0(destino, "/", ano_base, "/", arquivos), overwrite = TRUE)
   
 
 }
